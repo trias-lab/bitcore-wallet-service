9f41b5475de7e8ba699f78a042a40d4027bd1fdd
dasenlinCode <2845293434@qq.com>
2019-03-22 11:29:47 +0800

    add api doc

---
+++ api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "bws api",
+  "version": "0.1.0",
+  "description": "bitcore wallet service api",
+  "template": {
+    "withCompare": false
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-22T03:12:48.942Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
